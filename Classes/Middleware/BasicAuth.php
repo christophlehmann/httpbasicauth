5d8b0b75c9b9b04f2c18cd477855111a9d927207
Christoph Lehmann <christoph.lehmann@networkteam.com>
2024-05-12 23:30:59 +0200

    Beautification

--- Classes/Middleware/BasicAuth.php
+++ Classes/Middleware/BasicAuth.php
@@ -6,71 +6,111 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Http\HtmlResponse;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class BasicAuth implements MiddlewareInterface
 {
-    /**
-     * @param ServerRequestInterface $request
-     * @param RequestHandlerInterface $handler
-     * @return ResponseInterface
-     */
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        /** @var Site $site */
         $site = $request->getAttribute('site');
         if (!$site instanceof Site) {
             return $handler->handle($request);
         }
 
-        try {
-            if (!$site->getAttribute('basicauth_enabled')) {
-                return $handler->handle($request);
-            }
-        } catch (\InvalidArgumentException $e) {
-            // Attribute maybe not set after installing this extension?
+        if (!$this->isBasicAuthenticationEnabled($site)) {
             return $handler->handle($request);
         }
 
-        if ($site->getAttribute('basicauth_allow_devipmask')) {
-            $clientIPMatchesDevelopmentSystem = GeneralUtility::cmpIP(GeneralUtility::getIndpEnv('REMOTE_ADDR'),
-                $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask']);
-            if ($clientIPMatchesDevelopmentSystem) {
-                return $handler->handle($request);
-            }
+        if ($this->isAccessGrantedForDeveloperIps($site) && $this->isVisitorIpMatchingDeveloperIpMask()) {
+            return $handler->handle($request);
         }
 
-        if ($site->getAttribute('basicauth_allow_beuser')) {
-            $context = GeneralUtility::makeInstance(Context::class);
-            $backendUserLoggedIn = $context->getPropertyFromAspect('backend.user', 'id') > 0;
-            if ($backendUserLoggedIn) {
-                return $handler->handle($request);
-            }
+        if ($this->isAccessGrantedForBackendUsers($site) && $this->isVisitorABackendUser()) {
+            return $handler->handle($request);
         }
 
+        $authorizationHeaderCredentials = $this->getCredentialsFromAuthorizationHeader($request);
+        if ($authorizationHeaderCredentials && in_array($authorizationHeaderCredentials, $this->getCredentials($site))) {
+            return $handler->handle($request);
+        }
+
+        return new HtmlResponse('Not authorized', 401, ['WWW-Authenticate' => 'Basic realm="Not authorized"']);
+    }
+
+    protected function isBasicAuthenticationEnabled(Site $site): bool
+    {
+        try {
+            return (bool)$site->getAttribute('basicauth_enabled');
+        } catch (\InvalidArgumentException) {
+            // Attribute does not exist
+            return false;
+        }
+    }
+
+    protected function isAccessGrantedForDeveloperIps(Site $site): bool
+    {
+        try {
+            return (bool)$site->getAttribute('basicauth_allow_devipmask');
+        } catch (\InvalidArgumentException) {
+            // Attribute does not exist
+            return false;
+        }
+    }
+
+    protected function isVisitorIpMatchingDeveloperIpMask(): bool
+    {
+        return GeneralUtility::cmpIP(
+            GeneralUtility::getIndpEnv('REMOTE_ADDR'),
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask']
+        );
+    }
+
+    protected function isAccessGrantedForBackendUsers(Site $site): bool
+    {
+        try {
+            return (bool)$site->getAttribute('basicauth_allow_beuser');
+        } catch (\InvalidArgumentException) {
+            // Attribute does not exist
+            return false;
+        }
+    }
+
+    protected function isVisitorABackendUser(): bool
+    {
+        $context = GeneralUtility::makeInstance(Context::class);
+        return $context->getPropertyFromAspect('backend.user', 'id') > 0;
+    }
+
+    protected function getCredentialsFromAuthorizationHeader(ServerRequestInterface $request): ?string
+    {
         if (preg_match("/Basic\s+(.*)$/i", $request->getHeaderLine("Authorization"), $matches)) {
-            $credentials = explode(":", base64_decode($matches[1]), 2);
-
-            if (count($credentials) == 2) {
-
-                $basicauthCredentials = GeneralUtility::trimExplode(
-                    LF,
-                    $site->getAttribute('basicauth_credentials'),
-                    true
-                );
-                foreach ($basicauthCredentials ?? [] as $basicauthCredential) {
-                    if (GeneralUtility::trimExplode(':', $basicauthCredential, true, 2) === $credentials) {
-                        return $handler->handle($request);
-                    }
-                }
+            $credentials = base64_decode($matches[1]);
+            $usernamePasswordSeparator = ':';
+            if (str_contains($credentials, $usernamePasswordSeparator)
+                && !str_starts_with($credentials, $usernamePasswordSeparator)
+            ) {
+                return $credentials;
             }
         }
+        return null;
+    }
 
-        // @Todo: Return PSR-7 Response ?
-        header('WWW-Authenticate: Basic realm="Not authorized"');
-        header('HTTP/1.0 401 Unauthorized');
-        die("Not authorized");
+    protected function getCredentials(Site $site): array
+    {
+        try {
+            $credentials = GeneralUtility::trimExplode(
+                LF,
+                $site->getAttribute('basicauth_credentials'),
+                true
+            );
+            return $credentials;
+        } catch (\InvalidArgumentException) {
+            // Attribute does not exist
+            return [];
+        }
     }
 }
